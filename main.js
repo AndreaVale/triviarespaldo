7dfd5051be12298460283e7ad2b7670557001f26
AndreaVale <lain_iwakura55@hotmail.com>
2018-05-24 13:57:40 -0500

    Cuarta prueba

--- main.js
+++ main.js
@@ -16,29 +16,38 @@ if (gameplay == ("1")) {
 }
 if (firstQuestion == ("4")) {
 console.log("verdadero", rightAnswer = rightAnswer+1);
-} else {
-  firstQuestion !=("4")
-  console.log("falso", wrongAnswer= wrongAnswer+1);
-}
+document.getElementById("tableFirstRight").innerHTML = ("¿Cuánto es 2+2? Tú respondiste: " + firstQuestion);
 var secondQuestion = prompt("¿Cuánto es 5*1?");
-if (secondQuestion == ("5")) {
-  console.log("verdadero", rightAnswer = rightAnswer+1);
-} else {
-  secondQuestion !=("5")
-  console.log("falso", wrongAnswer = wrongAnswer+1);
-}
-var thirdQuestion = prompt("Cuánto es 1,000 - 500");
-if (thirdQuestion == ("500")) {
-  console.log("verdadero", rightAnswer = rightAnswer+1);
-} else {
-  (thirdQuestion !=("500"))
-  console.log("falso", wrongAnswer = wrongAnswer+1);
-}
-alert("¡¡¡Vamos a ver tus resultados!!!")
-if (true) {
-  document.getElementById("tableok").innerHTML = "¡¡¡CORRECTAS!!!";
-  document.getElementById("tablewrong").innerHTML = "¡¡¡MALAS!!!";
 
 }
+else {
+  firstQuestion !=("4")
+  console.log("falso", wrongAnswer = wrongAnswer+1);
+  document.getElementById("tablefirstWrong").innerHTML = ("¿Cuánto es 2+2? Tú respondiste: " + firstQuestion);
+  secondQuestion = prompt("¿Cuánto es 5*1?");
+}
+if (secondQuestion == ("5")) {
+  console.log("verdadero", rightAnswer = rightAnswer+1);
+  document.getElementById("tableSecondRight").innerHTML = ("¿Cuánto es 5*1? Tú respondiste: " + secondQuestion);
+  var thirdQuestion = prompt("¿Cuánto es 1,000-500?");
+}
+else {
+  secondQuestion !=("5")
+  console.log("falso", wrongAnswer = wrongAnswer+1);
+  document.getElementById("tableSecondWrong").innerHTML = ("¿Cuánto es 5*1? Tú respondiste:" + secondQuestion);
+  thirdQuestion = prompt("¿Cuánto es 1,000-500?");
+}
+if (thirdQuestion == ("500")) {
+  console.log("verdadero", rightAnswer = rightAnswer+1);
+  document.getElementById("tableThRight").innerHTML = ("¿Cuánto es 1,000-500? Tú respondiste: " + thirdQuestion);
+}
+else {
+  thirdQuestion !=("500")
+  console.log("falso", wrongAnswer = wrongAnswer+1);
+  document.getElementById("tableThirdWrong").innerHTML = ("¿Cuánto es 1,000-500? Tú respondiste:" + thirdQuestion);
+}
+alert("Vamos a ver tus resultados")
+document.getElementById("tableok").innerHTML = "¡¡¡Correctas!!!";
+document.getElementById("tablewrong").innerHTML = "¡¡¡Incorrectas!!!";
 }
 }
